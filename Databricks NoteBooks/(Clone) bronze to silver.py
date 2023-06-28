b3d8f0c5a7a74954ff478eeb2eff9697c636f48a
Tyrone Trebble <tctrebb@gmail.com>
2023-06-27 21:49:35 -0400

    Update (Clone) bronze to silver.py

--- Databricks NoteBooks/(Clone) bronze to silver.py
+++ Databricks NoteBooks/(Clone) bronze to silver.py
@@ -12,19 +12,24 @@ from pyspark.sql.functions import col, from_utc_timestamp, date_format, when
 from pyspark.sql.types import TimestampType
 import os
 
+#Declare table array
 table_name = []
 
+#Iterate through the ADLS bronze container and assign each folder to array
 for i in dbutils.fs.ls('mnt/bronze/dbo/'):
     table_name.append(i.name.split('/')[0])
 
+#Iterate through table array to load parquet files to data frame
 for i in table_name:
     path = '/mnt/bronze/dbo/' + i + '/'
     df = spark.read.format('parquet').load(path)
     column = df.columns
-    
+
+    #Transform date format in each parquet file to yyyy-MM-dd
     for col in column:
      if "Date" in col or "date" in col:
          df = df.withColumn(col, date_format(from_utc_timestamp(df[col].cast(TimestampType()),"UTC"), "yyyy-MM-dd"))
-         
+
+    #Set output path to silver container and save changes
     output_path = '/mnt/silver/dbo/' + i + '/'
     df.write.format('delta').mode("overwrite").save(output_path)
